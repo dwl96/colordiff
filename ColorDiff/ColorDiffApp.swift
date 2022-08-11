c58b957ad13bb7dd61b0e6cbbf9253c68baa62b8
Dan Lewis <dwl96@icloud.com>
2022-08-11 19:41:58 +0100

    Initial Commit

---
+++ ColorDiff/ColorDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorDiffApp.swift
+//  ColorDiff
+//
+//  Created by Dan Lewis on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
